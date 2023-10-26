acd23065b09b4cd76a7ac17d3e4599109bd3703c
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-10-27 08:44:18 +0900

    transformations script

--- transformations.py
+++ transformations.py
@@ -1,7 +1,18 @@
 # Import library
 from torchvision import transforms as T
 
-# Get transformations based on the input image dimensions
 def get_tfs(im_size = (224, 224), imagenet_normalization = True):
+
+    """
+    
+    This function gets several parameters and returns train and test transformations.
+
+    Parameters:
+
+        im_size                 - image dimension to be resized, tuple -> int;
+        imagenet_normalization  - whether or not to use ImageNet normalization, bool.
+            
+    """
+    
     mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
     return T.Compose([T.Resize(im_size), T.ToTensor(), T.Normalize(mean = mean, std = std)]) if imagenet_normalization else T.Compose([T.Resize(im_size), T.ToTensor()])
