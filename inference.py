5134b3d695881339b44a3d7bd8128ed4ca26d0f4
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-11-09 10:44:11 +0900

    inference script

--- inference.py
+++ inference.py
@@ -41,23 +41,22 @@ def run(args):
     print(f"The {args.model_name} state dictionary is successfully loaded!\n")
     # Get images, predictions, and labels
     all_ims, all_preds, all_gts = get_preds(model, test_dl, args.device)
-    
+    # Visualize the inference results
     visualize(all_ims, all_preds, all_gts, num_ims = 10, rows = 2, cls_names = cls_names, save_path = args.save_path, save_name = args.dataset_name)
+    # GradCAM of the inference results
     grad_cam(model, all_ims, num_ims = 10, rows = 2, save_path = args.save_path, save_name = args.dataset_name)
     
 if __name__ == "__main__":
     
     # Initialize Argument Parser    
-    parser = argparse.ArgumentParser(description = 'Image Classification Training Arguments')
+    parser = argparse.ArgumentParser(description = "Inference Process Arguments")
     
     # Add arguments to the parser
     parser.add_argument("-is", "--inp_im_size", type = tuple, default = (224, 224), help = "Input image size")
-    parser.add_argument("-dn", "--dataset_name", type = str, default = 'cars', help = "Dataset name for training")
-    parser.add_argument("-mn", "--model_name", type = str, default = 'rexnet_150', help = "Model name for backbone")
-    parser.add_argument("-d", "--device", type = str, default = 'cuda:3', help = "GPU device name")
-    # parser.add_argument("-mn", "--model_name", type = str, default = 'vit_base_patch16_224', help = "Model name for backbone")
-    # parser.add_argument("-mn", "--model_name", type = str, default = 'vgg16_bn', help = "Model name for backbone")
-    parser.add_argument("-sm", "--save_model_path", type = str, default = 'saved_models', help = "Path to the directory to save a trained model")
+    parser.add_argument("-dn", "--dataset_name", type = str, default = "cars", help = "Dataset name for training")
+    parser.add_argument("-mn", "--model_name", type = str, default = "rexnet_150", help = "Model name for backbone")
+    parser.add_argument("-d", "--device", type = str, default = "cuda:3", help = "GPU device name")
+    parser.add_argument("-sm", "--save_model_path", type = str, default = "saved_models", help = "Path to the directory to save a trained model")
     parser.add_argument("-sp", "--save_path", type = str, default = "results", help = "Path to dir to save inference results")
     parser.add_argument("-dl", "--dls_dir", type = str, default = "saved_dls", help = "Path to dir to save dataloaders")
     
